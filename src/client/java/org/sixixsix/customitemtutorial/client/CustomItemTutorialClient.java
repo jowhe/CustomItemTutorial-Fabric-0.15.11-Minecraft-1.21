f65e521ee2f24b2184e970d0dfef9f29751b2eba
joe <jjhowe@protonmail.ch>
2024-08-04 18:23:40 +0100

    Initial Commit

---
+++ src/client/java/org/sixixsix/customitemtutorial/client/CustomItemTutorialClient.java
@@ -0,0 +1,10 @@
+package org.sixixsix.customitemtutorial.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomItemTutorialClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/sixixsix/customitemtutorial/CustomItemTutorial.java
@@ -0,0 +1,10 @@
+package org.sixixsix.customitemtutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CustomItemTutorial implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
